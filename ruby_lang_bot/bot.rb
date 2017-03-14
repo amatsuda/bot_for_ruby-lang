f5133a2e3c0aeace74c2f02c79585dd62de4d1ac
Akira Matsuda <ronnie@dio.jp>
2017-03-15 04:50:22 +0900

    The bot matches "Bugs#10000" and says https://bugs.ruby-lang.org/issues/10000

--- ruby_lang_bot/bot.rb
+++ ruby_lang_bot/bot.rb
@@ -1,5 +1,8 @@
 # frozen_string_literal: true
 module RubyLangBot
   class Bot < SlackRubyBot::Bot
+    match /bugs? ?#(?<num>\d*)/i do |client, data, match|
+      client.say channel: data.channel, text: "https://bugs.ruby-lang.org/issues/#{match[:num]}"
+    end
   end
 end
